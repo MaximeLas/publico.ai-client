4eacb017879bde02466a645149d78436ee4f2834
JJ Velez <josevelezq7@gmail.com>
2023-09-02 23:20:47 -0700

    Add Questions section to home; Delete unused image

---
+++ src/pages/home/Questions.tsx
@@ -0,0 +1,9 @@
+const Questions = () => (
+  <div className="questions center-align">
+    <h1>Questions?</h1>
+    <br></br>
+    <p>We'd love to hear from you! Contact us at info@publico.ai.</p>
+  </div>
+);
+
+export default Questions;
