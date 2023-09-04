cff490dd2c5efb7550931425b566010198a72db6
JJ Velez <josevelezq7@gmail.com>
2023-09-04 13:49:59 -0700

    Show previews

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -2,7 +2,8 @@ const Contact = () => {
   return (
     <div className="how-it-works">
       <div className="how-it-works-content">
-        <h1 className="big-text">Contact</h1>
+        <h1 className="big-text">Conasdfsadtact</h1>
+        <p>abcsdfas;ldkfj</p>
       </div>
     </div>
   );
