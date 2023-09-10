767fd2ba2778d5b8cacf8d8496a11b8d01b4c0f8
JJ Velez <josevelezq7@gmail.com>
2023-09-10 00:46:08 -0700

    Embed Publico EC2-hosted demo in website

--- src/pages/try-it/TryIt.tsx
+++ src/pages/try-it/TryIt.tsx
@@ -1,8 +1,15 @@
+import "./TryIt.css";
+
 const TryIt = () => {
   return (
-    <div className="how-it-works">
-      <div className="how-it-works-content">
-        <h1 className="big-text">Try It</h1>
+    <div className="try-it">
+      <h1 className="center-align medium-text">Try it below!</h1>
+      <div className="publico-demo-iframe-container">
+        <iframe
+          className="publico-demo-iframe"
+          src="http://ec2-54-153-106-76.us-west-1.compute.amazonaws.com:7860/"
+          title="Publico.ai Demo App"
+        ></iframe>
       </div>
     </div>
   );
