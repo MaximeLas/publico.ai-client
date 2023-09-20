6bdc4aae613906eae359f0114e98388a31511b79
Maxime Las <maximelas1@gmail.com>
2023-09-20 23:54:42 +0100

    Redirect Start a new grant to try-it page

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -5,10 +5,13 @@ import { HiOutlineDocumentSearch } from "react-icons/hi";
 import { IoDocumentsOutline } from "react-icons/io5";
 import { MdAccountBox } from "react-icons/md";
 import { MdContactSupport } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+
   const BUTTONS = [
-    { text: "Start a new grant", Icon: TfiWrite },
+    { text: "Start a new grant", Icon: TfiWrite, action: () => navigate('/try-it') },
     { text: "My previous grants", Icon: HiOutlineDocumentSearch },
     { text: "My Documents", Icon: IoDocumentsOutline },
     { text: "My Account", Icon: MdAccountBox },
