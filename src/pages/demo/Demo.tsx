6ee0fd81fa67a63b9a586f1761383a678d2eead7
Maxime Las <maximelas1@gmail.com>
2023-10-04 19:10:47 +0100

    Fix most recent link

--- src/pages/demo/Demo.tsx
+++ src/pages/demo/Demo.tsx
@@ -7,7 +7,7 @@ const Demo: React.FC = () => {
       <div className="publico-demo-iframe-container">
         <iframe
           className="publico-demo-iframe"
-          src="https://f0436ababe30ce8cac.gradio.live"
+          src="https://c159a9ca0a3f1015ef.gradio.live"
           title="Publico.ai Demo App"
         ></iframe>
       </div>
