a4d21cac36c27dc74515246953983bd1af4a3754
Manikanta Sangu <manikanta.sangu@gmail.com>
2024-01-05 04:55:19 +0000

    Adding password less authenitication using firebase

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -6,18 +6,62 @@ import MySpinner from "../../components/spinner/MySpinner";
 import useAuth from "../../auth/useAuth";
 import { useNavigate } from "react-router-dom";
 import { LoginInfo } from "../../auth/auth";
+import { getAuth, isSignInWithEmailLink, signInWithEmailLink } from "firebase/auth";
+import { useEffect } from "react";
 
 const Login: React.FC = () => {
   const auth = useAuth();
+  const firebaseAuth = getAuth();
   const navigate = useNavigate();
 
+  useEffect(()=>{
+    if(auth?.user){
+      // user is already signed in
+      navigate('/dashboard');
+    }
+    else {
+      if (isSignInWithEmailLink(firebaseAuth, window.location.href)) {
+        console.log("Reached here");
+        // Additional state parameters can also be passed via URL.
+        // This can be used to continue the user's intended action before triggering
+        // the sign-in operation.
+        // Get the email if available. This should be available if the user completes
+        // the flow on the same device where they started it.
+        let email = localStorage.getItem('emailForSignIn');
+        console.log(email);
+        if (!email) {
+          // User opened the link on a different device. To prevent session fixation
+          // attacks, ask the user to provide the associated email again. For example:
+          email = window.prompt('Please provide your email for confirmation');
+        }
+        // The client SDK will parse the code from the link for you.
+        if (email) {
+          signInWithEmailLink(firebaseAuth, email, window.location.href)
+            .then((result) => {
+              // Clear email from storage.
+              localStorage.removeItem('emailForSignIn');
+              // You can access the new user via result.user
+              // Additional user info profile not available via:
+              // result.additionalUserInfo.profile == null
+              // You can check if the user is new or existing:
+              // result.additionalUserInfo.isNewUser
+              auth?.setCurrentUser(result.user.uid);
+              navigate("/dashboard");
+            })
+            .catch((error) => {
+              // Some error occurred, you can inspect the code: error.code
+              // Common errors could be invalid email and invalid or expired OTPs.
+            });
+        }
+      }
+    }
+  });
   const onSubmit = async (values: LoginInfo, actions: any) => {
     console.log("submitted form: ", values);
     console.log(auth);
 
     await auth?.login(values);
     actions.resetForm();
-    navigate("/dashboard");
   };
 
   // Good formik tutorial - https://www.youtube.com/watch?v=7Ophfq0lEAY
@@ -66,22 +110,6 @@ const Login: React.FC = () => {
               />
               <Form.Control.Feedback type="invalid">
                 {touched.email && errors.email}
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group className="mb-3 form-group">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                name="password"
-                type="password"
-                placeholder="Enter password"
-                value={values.password}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                required
-                isInvalid={touched.password && !!errors.password}
-              />
-              <Form.Control.Feedback type="invalid">
-                {touched.password && errors.password}
               </Form.Control.Feedback>
             </Form.Group>
           </Form>
