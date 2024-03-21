cde4fce33735aad7dfe82fe3a8f9ad77af3b9c7d
LiorZana <liorzana@gmail.com>
2024-03-21 22:59:01 +0200

    Commented out store persistence until the "new session" button is implemented

--- src/state/store.ts
+++ src/state/store.ts
@@ -3,17 +3,17 @@ import createChatSlice from "./slices/ChatSlice";
 import createQuestionsSlice from "./slices/GuidingQuestionsSlice";
 import createUserSlice from "./slices/UserSlice";
 import { RootState } from "./types";
-import { persist } from "zustand/middleware";
+// import { persist } from "zustand/middleware";
 
 const store = createStore<RootState>()(
-  persist<RootState>(
+  // persist<RootState>(
     (...args) => ({
       ...createChatSlice(...args),
       ...createQuestionsSlice(...args),
       ...createUserSlice(...args),
     }),
-    { name: "rootStore" }
-  )
+  //   { name: "rootStore" }
+  // )
 );
 
 export default store;
