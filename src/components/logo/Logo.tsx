d0a14c8763259e327595f4b1f01886dcf45a12db
LiorZana <liorzana@gmail.com>
2024-04-07 23:03:39 +0300

    Create Logo.tsx

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LogoImg from "../../assets/logo/logo.png";
+
+function Logo() {
+  return <img width="150px" height="auto" src={LogoImg} alt="Publico logo" />;
+}
+
+export default Logo;
