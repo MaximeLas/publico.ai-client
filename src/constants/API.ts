d85290a24c8506c40c43a1b79da3c5e16bd26cf5
Maxime Las <maximelas1@gmail.com>
2024-04-28 21:48:58 +0100

    Add App Engine URL as PROD_API_HOSTNAME

--- src/constants/API.ts
+++ src/constants/API.ts
@@ -1,9 +1,6 @@
 
 export const LOCAL_API_HOSTNAME = "http://127.0.0.1:8000"
 
-// export const PROD_API_HOSTNAME = "https://..."
+export const PROD_API_HOSTNAME = "https://publico-ai.ew.r.appspot.com"
 
-// uncomment when you have a production API
-// export const API_HOSTNAME = process.env.NODE_ENV === "production" ? "https://..." : LOCAL_API_HOSTNAME
-
-export const API_HOSTNAME = LOCAL_API_HOSTNAME;
+export const API_HOSTNAME = process.env.NODE_ENV === "production" ? PROD_API_HOSTNAME : LOCAL_API_HOSTNAME
