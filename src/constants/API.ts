cd17c7247e04b45527e234b377253cd14e84a26c
Maxime Las <maximelas1@gmail.com>
2024-04-28 22:44:22 +0100

    Dummy change

--- src/constants/API.ts
+++ src/constants/API.ts
@@ -1,6 +1,4 @@
 
 export const LOCAL_API_HOSTNAME = "http://127.0.0.1:8000"
-
 export const PROD_API_HOSTNAME = "https://publico-ai.ew.r.appspot.com"
-
 export const API_HOSTNAME = process.env.NODE_ENV === "production" ? PROD_API_HOSTNAME : LOCAL_API_HOSTNAME
